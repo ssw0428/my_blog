26389c83462087c7e0e4d13d610433df1f713d02
ssw0428 <ssw0428@naver.com>
2020-06-15 11:22:54 +0900

    모바일 탑바 작업

--- part/head.php
+++ part/head.php
@@ -6,20 +6,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>블로그</title>
     <link rel="stylesheet" href="/resource/common.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/resource/common.js"></script>
 </head>
 
 <body>
-
     <div class="top-bar">
+    <div class="mobile-top-bar visible-sm-down flex">
+        <a href="#" class="btn-toggle-mobile-side-bar flex-as-c">
+            <div></div>
+            <div></div>
+            <div></div>
+        </a>
+    </div>
+    
+    <div class="top-bar visible-md-up">
         <div class="con height-100p flex">
+            <a href="/" class="logo flex flex-ai-c">
+                <i class="fas fa-clone"></i>
+            </a>
             <nav class="menu-box-1 flex flex-1-0-0">
                 <ul class="flex flex-1-0-0">
-                    <li class="flex-1-0-0 flex"><a class="flex flex-1-0-0 flex-jc-c flex-ai-c" href="/"><span>Home</span></a></li>
-                    <li class="flex-1-0-0 flex"><a class="flex flex-1-0-0 flex-jc-c flex-ai-c" href="/list.php"><span>Article</span></a></li>
-                    <li class="flex-1-0-0 flex"><a class="flex flex-1-0-0 flex-jc-c flex-ai-c" href="/aboutMe.php"><span>About Me</span></a></li>
-                    <li class="flex-1-0-0 flex"><a class="flex flex-1-0-0 flex-jc-c flex-ai-c" href="#"><span>SNS</span></a></li>
+                    <li class="flex-1-0-0 flex"><a href="/" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Home</a></li>
+                    <li class="flex-1-0-0 flex"><a href="/list.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Articles</a>
+                    </li>
+                    <li class="flex-1-0-0 flex"><a href="/aboutMe.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">About Me</a>
+                    </li>
+                    <li class="flex-1-0-0 flex">
+                        <a href="#" class="flex flex-1-0-0 flex-ai-c flex-jc-c">SNS</a>
+                        <ul>
+                            <li>
+                                <a href="https://github.com/jhs512" target="_blank">
+                                    GITHUB
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/jhs512" target="_blank">
+                                    FACEBOOK
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/jhs512" target="_blank">
+                                    INSTA
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
                 </ul>
             </nav>
         </div>
+    </div>  
 
---
+++ public/resource/common.js
@@ -0,0 +1,18 @@
+function MobileSideBar__toggle() {
+    var $btn = $('.btn-toggle-mobile-side-bar');
+
+    if ( $btn.hasClass('active') ) {
+        $btn.removeClass('active');
+    }
+    else {
+        $btn.addClass('active');
+    }
+}
+
+function MobileSideBar__init() {
+    $('.btn-toggle-mobile-side-bar').click(MobileSideBar__toggle);
+}
+
+$(function() {
+    MobileSideBar__init();
+}); 
