a63c876d36e691fdfdda77da71cad18a580d6144
ssw0428 <ssw0428@naver.com>
2020-07-16 18:46:36 +0900

    한컴 다시 하기

--- public/Portfolio/hancomm/index.js
+++ public/Portfolio/hancomm/index.js
@@ -15,8 +15,102 @@ $(function () {
 	MySlider1__init();
 });
 
+
 $(function () {
 	window.addEventListener('load', function (event) {
 		AOS.init();
 	});
+});
+
+$(function () {
+	function MySlider1__updateCurrentPageNumber(event) {
+		var $owl = $(event.target);
+		var index = $owl.find('.owl-item.active').first().find(' > .item').attr('data-no');
+		index = parseInt(index);
+
+		var $currentIndex = $owl.parent().parent().find('.controler > .control-box > .index-box > .current-index');
+
+		$currentIndex.text(index + 1);
+	}
+
+	function MySlider1__init() {
+		var owlNowSliding = false;
+		var slidingDuration = 1000;
+
+		$('.my-slider-1').each(function (index, el) {
+			var $mySlider1 = $(el);
+			var $item = $mySlider1.find('.item');
+			totalCount = $item.length;
+
+			$item.each(function (index, itemEl) {
+				var $itemEl = $(itemEl);
+				$itemEl.attr('data-no', $itemEl.index());
+			});
+
+			var $totalCount = $mySlider1.find('> .head >  .control-box > .index-box > .total-count');
+			$totalCount.text(totalCount);
+		});
+
+		var owl = $('.my-slider-1 .owl-carousel').owlCarousel({
+			loop: true,
+			items: 6,
+			smartSpeed: slidingDuration,
+			onInitialized: MySlider1__updateCurrentPageNumber,
+			onTranslated: MySlider1__updateCurrentPageNumber,
+		});
+
+		owl.on('mousewheel', '.owl-stage', function (e) {
+			e.preventDefault();
+
+			if (owlNowSliding) {
+				return;
+			}
+
+			owlNowSliding = true;
+
+			var deltaY = e.deltaY;
+
+			if (deltaY == undefined) {
+				deltaY = e.originalEvent.wheelDelta;
+			}
+
+			if (deltaY < 0) {
+				owl.trigger('next.owl');
+			} else {
+				owl.trigger('prev.owl');
+			}
+
+			setTimeout(function () {
+				owlNowSliding = false;
+			}, slidingDuration);
+		});
+
+		$('.my-slider-1 > .head > .control-box > .nav-box > span').click(function () {
+			if (owlNowSliding) {
+				return;
+			}
+
+			owlNowSliding = true;
+
+			var $clicked = $(this);
+
+			if ($clicked.is(':first-child')) {
+				owl.trigger('prev.owl');
+			} else {
+				owl.trigger('next.owl');
+			}
+
+			setTimeout(function () {
+				owlNowSliding = false;
+			}, slidingDuration);
+		});
+
+		setTimeout(function () {
+			$('.my-slider-1').addClass('active');
+		}, 1500);
+	}
+
+	$(function () {
+		MySlider1__init();
+	})
 });
--- public/Portfolio/hancomm/index.php
+++ public/Portfolio/hancomm/index.php
@@ -22,11 +22,11 @@
 
     <script src="http://www.youtube.com/player_api"></script>
     <script src="/Portfolio/hancomm/index.js"></script>
-    
+
 </head>
 
 <body>
-    <header class="main">
+    <header class="main" data-aos="fade-down" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
         <div class="inner">
             <nav class="menu-bar flex flex-jc-end con">
                 <ul class="flex">
@@ -53,90 +53,97 @@
         <a class="left-about-us con" href="#">
             <span>ABOUT US</span>
         </a>
-        <div class="hancomm-title con">
+        <div class="hancomm-title con" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="800"
+            data-aos-offset="0">
             <img src="https://ssw0428.github.io/img1/pf/hancomm/logo.png" alt="">
         </div>
-        <div data-aos="flip-left" class="click">
+        <div data-aos="fade-right" data-aos-delay="1000" class="click">
             <span class="name">More Work</span>
             <span class="arrow"></span>
         </div>
-        <div class="main-control aos-init aos-animate flex">
-            <div class="swiper-pagination eng swiper-pagination-fraction flex">
-                <span class="swiper-pagination-current">1</span>
-                /
-                <span class="swiper-pagination-total">6</span>
-            </div>
-            <div class="dp-inblock flex flex-jc-end">
-                <div class="control-prev eng">PREV</div>
-                <div class="slash">/</div>
-                <div class="control-next eng">NEXT</div>
-            </div>
-        </div>
+
         <div class="my-slider-1">
-            <div class="owl-carousel owl-theme">
-                <div class="item">
-                    <div class="slide-list">
-                        <a href="" class="title">
-                            <span class="name1">CAMPAIGN</span>
-                            <span class="name2">KEB하나은행 하나원큐</span>
-                            <span class="name3"></span>
-                        </a>
-                        <img src="https://ssw0428.github.io/img1/pf/hancomm/200422_하나은행_하나원큐.jpg" alt="">
+            <div class="controler" data-aos="fade-left" data-aos-delay="1000">
+                <div class="control-box flex">
+                    <div class="index-box flex">
+                        <span class="current-index">1</span>
+                        /
+                        <span class="total-count"></span>
+                    </div>
+                    <div class="nav-box flex flex-jc-end">
+                        <span class="btn-left">PREV</span>
+                        <div class="slash">/</div>
+                        <span class="btn-right">NEXT</span>
                     </div>
                 </div>
-                <div class="item">
-                    <div class="slide-list">
-                        <a href="" class="title">
-                            <span class="name1">CAMPAIGN</span>
-                            <span class="name2">오로나민C 큰소리 뻥뻥 헬스장</span>
-                            <span class="name3"></span>
-                        </a>
-                        <img src="https://ssw0428.github.io/img1/pf/hancomm/200406_오로나민C_큰소리뻥뻥_헬스장.jpg" alt="">
+            </div>
+            <div class="inner">
+                <div class="owl-carousel owl-theme">
+                    <div class="item">
+                        <div class="slide-list">
+                            <a href="" class="title">
+                                <span class="name1">CAMPAIGN</span>
+                                <span class="name2">KEB하나은행 하나원큐</span>
+                                <span class="name3"></span>
+                            </a>
+                            <div class="img-box" style="background-image:url(https://ssw0428.github.io/img1/pf/hancomm/200422_하나은행_하나원큐.jpg);"></div>
+                        </div>
                     </div>
-                </div>
-                <div class="item">
-                    <div class="slide-list">
-                        <a href="" class="title">
-                            <span class="name1">CAMPAIGN</span>
-                            <span class="name2">오로나민C 큰소리 뻥뻥 부부</span>
-                            <span class="name3"></span>
-                        </a>
-                        <img src="https://ssw0428.github.io/img1/pf/hancomm/200406_오로나민C_큰소리뻥뻥_부부.jpg" alt="">
+                    <div class="item">
+                        <div class="slide-list">
+                            <a href="" class="title">
+                                <span class="name1">CAMPAIGN</span>
+                                <span class="name2">오로나민C 큰소리 뻥뻥 헬스장</span>
+                                <span class="name3"></span>
+                            </a>
+                            <div class="img-box" style="background-image:url(https://ssw0428.github.io/img1/pf/hancomm/200406_오로나민C_큰소리뻥뻥_헬스장.jpg);"></div>
+                        </div>
                     </div>
-                </div>
-                <div class="item">
-                    <div class="slide-list">
-                        <a href="" class="title">
-                            <span class="name1">CAMPAIGN</span>
-                            <span class="name2">참이슬 이슬방울</span>
-                            <span class="name3"></span>
-                        </a>
-                        <img src="https://ssw0428.github.io/img1/pf/hancomm/200327_하이트진로_참이슬 이슬방울.jpg" alt="">
+                    <div class="item">
+                        <div class="slide-list">
+                            <a href="" class="title">
+                                <span class="name1">CAMPAIGN</span>
+                                <span class="name2">오로나민C 큰소리 뻥뻥 부부</span>
+                                <span class="name3"></span>
+                            </a>
+                            <div class="img-box" style="background-image:url(https://ssw0428.github.io/img1/pf/hancomm/200406_오로나민C_큰소리뻥뻥_부부.jpg);"></div>
+                        </div>
                     </div>
-                </div>
-                <div class="item">
-                    <div class="slide-list">
-                        <a href="" class="title">
-                            <span class="name1">CAMPAIGN</span>
-                            <span class="name2">골드킹 윙 윙 포인트 레슨</span>
-                            <span class="name3"></span>
-                        </a>
-                        <img src="https://ssw0428.github.io/img1/pf/hancomm/re_200201_bhc골드킹윙.jpg" alt="">
+                    <div class="item">
+                        <div class="slide-list">
+                            <a href="" class="title">
+                                <span class="name1">CAMPAIGN</span>
+                                <span class="name2">참이슬 이슬방울</span>
+                                <span class="name3"></span>
+                            </a>
+                            <div class="img-box" style="background-image:url(https://ssw0428.github.io/img1/pf/hancomm/200327_하이트진로_참이슬 이슬방울.jpg);"></div>
+                        </div>
                     </div>
-                </div>
-                <div class="item">
-                    <div class="slide-list">
-                        <a href="" class="title">
-                            <span class="name1">CAMPAIGN</span>
-                            <span class="name2">야나두 자동차B</span>
-                            <span class="name3"></span>
-                        </a>
-                        <img src="https://ssw0428.github.io/img1/pf/hancomm/1588730145857_191228_야나두_자동차B국문.jpg" alt="">
+                    <div class="item">
+                        <div class="slide-list">
+                            <a href="" class="title">
+                                <span class="name1">CAMPAIGN</span>
+                                <span class="name2">골드킹 윙 윙 포인트 레슨</span>
+                                <span class="name3"></span>
+                            </a>
+                            <div class="img-box" style="background-image:url(https://ssw0428.github.io/img1/pf/hancomm/re_200201_bhc골드킹윙.jpg);"></div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="slide-list">
+                            <a href="" class="title">
+                                <span class="name1">CAMPAIGN</span>
+                                <span class="name2">야나두 자동차B</span>
+                                <span class="name3"></span>
+                            </a>
+                            <div class="img-box" style="background-image:url(https://ssw0428.github.io/img1/pf/hancomm/1588730145857_191228_야나두_자동차B국문.jpg);"></div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 </body>
 
 </html>
