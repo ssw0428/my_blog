a092d3f21c570dd0409e5ba9822f488081e8dde0
ssw0428 <ssw0428@naver.com>
2020-07-17 18:39:24 +0900

    포트폴리오

--- public/Portfolio/index.php
+++ public/Portfolio/index.php
@@ -6,12 +6,25 @@
     <title>Document</title>
 </head>
 <body>
+<h1>코딩 작업물</h1>
     <ul>
-        <li><a href="/portfolio/k2">k2 safety</a></li>
+        <li>
+        <a href="/portfolio/k2">k2 safety</a>
+        <a href="">원본사이트</a>
+        </li>
         <li><a href="/portfolio/hancomm">한컴 종합광고대행사</a></li>
     </ul>
+<h1>디자인 작업물</h1>
     <ul>
-        <li><a href="https://ssw0428.github.io/img1/pf/artworks/1/design-1.jpg">디자인 작업물1</a></li>
+        <li>
+        <a href="https://ssw0428.github.io/img1/pf/artworks/1/design-1.jpg">디자인 작업물1</a>
+        <ul>
+            <li>창작여부: 모작 or 창작</li>
+        </ul>
+        <ul>
+            <li>출처: 망고보드(링크)</li>
+        </ul>
+        </li>
     </ul>
 </body>
 </html>
