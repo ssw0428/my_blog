587f40e2b25452d433b9c4cd7f3b28cf3f42254c
ssw0428 <ssw0428@naver.com>
2020-07-28 18:48:01 +0900

    새로운 claster

--- public/Portfolio/claster/index.js
+++ public/Portfolio/claster/index.js
@@ -1,3 +1,30 @@
-$ (function() {
-    alert("hi");
+$(function () {
+    $(document).ready(function () {
+        $('#fullpage').fullpage({
+            scrollBar: true,
+            navigation: true,
+
+        });
+    });
+});
+
+$(function () {
+    var swiper = new Swiper('.swiper1', {
+        autoplay: {
+            delay: 8500,
+            disableOnInteraction: false,
+        },
+        slidesPerView: 1,
+        loop: true
+    });
+});
+
+$(function() {
+    var SideMenuBar__hide = function() {
+        $('.side-menu-bar').css('left', ''); 
+      }
+      
+      var SideMenuBar__show = function() {
+        $('.side-menu-bar').css('left', '0');
+      }
 });
--- public/Portfolio/claster/index.php
+++ public/Portfolio/claster/index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>claster</title>
 
     <link rel="stylesheet" href="/portfolio/claster/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <!-- 제이쿼리 불러오기 -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Full page 불러오기 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.css">
 
     <!-- swiper 불러오기 -->
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
@@ -16,9 +21,111 @@
 
     <script src="/portfolio/claster/index.js"></script>
 </head>
+
 <body>
-    <div>
-    하하
+    <div class="fullpage" id="fullpage">
+        <div class="top-menu-box flex">
+            <div onclick="SideMenuBar__show();" class="menu-box">
+                <img src="https://ssw0428.github.io/img1/pf/claster/icon_menu_white.png" alt="">
+            </div>
+            <div class="logo-box">
+                <a class="logo">
+                    <img src="https://ssw0428.github.io/img1/pf/claster/logo_white.png" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="side-menu-bar">
+            <div class="click-btn">
+                <div class="btn-close" onclick="SideMenuBar__hide();">
+                    <img src="https://ssw0428.github.io/img1/pf/claster/button_close.png" alt="">
+                </div>
+            </div>
+            <ul>
+                <li><a href="">works</a></li>
+                <li><a href="">business</a></li>
+                <li><a href="">social+</a></li>
+                <li><a href="">contact</a></li>
+            </ul>
+            <span><a href="">COMPANY BROCHURE</a></span>
+            <span><a href="">RECRUIT</a></span>
+        </div>
+        <div class="section page1">
+            <div class="swiper1">
+                <ul class="swiper-wrapper">
+                    <li class="swiper-slide">
+                        <div class="text-box">
+                            <a class="text-list" href="">
+                                <span>FILA</span>
+                                <span>휠라</span>
+                                <span>카카오 플친 운영</span>
+                                <span>플러스친구 디자인 및 AD광고 대행</span>
+                            </a>
+                        </div>
+                        <div class="img-box">
+                            <img src="https://ssw0428.github.io/img1/pf/claster/6d61696e2d62616e6e65725f3031.jpg"
+                                alt="">
+                        </div>
+                    </li>
+                    <li class="swiper-slide">
+                        <div class="text-box">
+                            <a class="text-list" href="">
+                                <span>VDIVOV</span>
+                                <span>비디보브</span>
+                                <span>웹페이지 구축</span>
+                                <span>웹페이지 구축 및 상세페이지 운영</span>
+                            </a>
+                        </div>
+                        <div class="img-box">
+                            <img src="https://ssw0428.github.io/img1/pf/claster/6d61696e2d62616e6e65725f3032.jpg"
+                                alt="">
+                        </div>
+                    </li>
+                    <li class="swiper-slide">
+                        <div class="text-box">
+                            <a class="text-list" href="">
+                                <span>SUM37˚</span>
+                                <span>숨37˚</span>
+                                <span>홈페이지 구축</span>
+                                <span>온라인 웹 사이트 & 모바일 리뉴얼 구축</span>
+                            </a>
+                        </div>
+                        <div class="img-box">
+                            <img src="https://ssw0428.github.io/img1/pf/claster/6d61696e2d62616e6e65725f3033.jpg"
+                                alt="">
+                        </div>
+                    </li>
+                    <li class="swiper-slide">
+                        <div class="text-box">
+                            <a class="text-list" href="">
+                                <span>SGMALL</span>
+                                <span>스팍스 글렌코</span>
+                                <span>광고대행</span>
+                                <span>온라인 AD 및 이벤트 대행 & 운영</span>
+                            </a>
+                        </div>
+                        <div class="img-box">
+                            <img src="https://ssw0428.github.io/img1/pf/claster/6d61696e2d62616e6e65725f30345f32.jpg"
+                                alt="">
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="section page2">
+            <div class="text-box">
+                <div class="text-list2">
+                    <span>creating</span>
+                    <span>a unique</span>
+                    <span>future</span>
+                    <span>who are we?</span>
+                </div>
+            </div>
+        </div>
+        <div class="section page3">
+        </div>
+        <div class="section page4">
+        </div>
     </div>
 </body>
+
 </html>
