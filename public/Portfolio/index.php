8a2c09ff7dcd06cbbdf710e17fdfdc2deef334bd
ssw0428 <ssw0428@naver.com>
2020-08-04 18:44:27 +0900

    d

--- public/Portfolio/index.php
+++ public/Portfolio/index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<h1>코딩 작업물</h1>
+    <h1>코딩 작업물</h1>
     <ul>
         <li>
-        <a href="/portfolio/k2">k2 safety</a>
-        <a href="http://www.k2safety.co.kr/main/index.php">원본사이트</a>
+            <a href="/portfolio/k2">k2 safety</a>
+            <a href="http://www.k2safety.co.kr/main/index.php">원본사이트</a>
         </li>
         <li>
             <a href="/portfolio/hancomm">한컴 종합광고대행사</a>
@@ -25,27 +27,45 @@
             <a href="http://claster.co.kr/">원본사이트</a>
         </li>
     </ul>
-<h1>디자인 작업물</h1>
+    <h1>디자인 작업물</h1>
     <ul>
         <li>
-        <a href="https://ssw0428.github.io/img1/pf/artworks/1/design-1.jpg">디자인 작업물1</a>
-        <ul>
-            <li>창작여부: 모작</li>
-        </ul>
-        <ul>
-            <li>출처: 망고보드(링크)</li>
-        </ul>
+            <a href="https://ssw0428.github.io/img1/pf/artworks/1/design-1.jpg">디자인 작업물1</a>
+            <ul>
+                <li>창작여부: 모작</li>
+            </ul>
+            <ul>
+                <li>출처: 망고보드(링크)</li>
+            </ul>
         </li>
     </ul>
     <ul>
         <li>
-        <a href="https://ssw0428.github.io/img1/pf/artworks/1/design-2.jpg">디자인 작업물2</a>
-        <ul>
-            <li>창작여부: 창작</li>
-        </ul>
-        <ul>
-        </ul>
+            <a href="https://ssw0428.github.io/img1/pf/artworks/1/design-2.jpg">디자인 작업물2</a>
+            <ul>
+                <li>창작여부: 창작</li>
+            </ul>
+            <ul>
+            </ul>
+        </li>
+    </ul>
+    <ul>
+        <li>
+            <a href="https://ssw0428.github.io/img1/pf/artworks/1/app-1.png">앱작업물</a>
+            <br>
+            <a href="https://ssw0428.github.io/img1/pf/artworks/1/app-2.png">앱작업물</a>
+            <br>
+            <a href="https://ssw0428.github.io/img1/pf/artworks/1/app-2-1.png">앱작업물</a>
+            <br>
+            <a href="https://ssw0428.github.io/img1/pf/artworks/1/app-3.png">앱작업물</a>
+            <br>
+            <a href="https://ssw0428.github.io/img1/pf/artworks/1/app-3-1.png">앱작업물</a>
+            <br>
+            <a href="https://ssw0428.github.io/img1/pf/artworks/1/app-4.png">앱작업물</a>
+            <br>
+            <a href="https://ssw0428.github.io/img1/pf/artworks/1/app-4-1.png">앱작업물</a>
         </li>
     </ul>
 </body>
+
 </html>
