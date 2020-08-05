272ee1f0d60b7dab7405bbf2be84e143cf8c0fb3
ssw0428 <ssw0428@naver.com>
2020-08-05 11:00:48 +0900

    remian

---
+++ public/Portfolio/raemian/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXPERIENCE of PRIDE 래미안</title>
+
+    <link rel="stylesheet" href="/portfolio/raemian/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <!-- 제이쿼리 불러오기 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- swiper 불러오기 -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+    <script src="/portfolio/raemian/index.js"></script>
+</head>
+
+<body>
+    <div class="header">
+        <div class="header-bar flex">
+            <h1>
+                <div class="logo-box">
+                    <img src="https://ssw0428.github.io/img1/pf/apt/logo_h.png" alt="">
+                </div>
+            </h1>
+            <div class="left-button">
+                <img src="https://ssw0428.github.io/img1/pf/apt/btn_h_sitemap.png" alt="">
+            </div>
+            <nav class="gnb-bar">
+                <ul class="flex">
+                    <li>
+                        <a href="">단지정보</a>
+                    </li>
+                    <li>
+                        <a href="">갤러리</a>
+                    </li>
+                    <li>
+                        <a href="">래미안타임즈</a>
+                    </li>
+                    <li>
+                        <a href="">브랜드</a>
+                    </li>
+                    <li>
+                        <a href="">분양안내</a>
+                    </li>
+                    <li>
+                        <a href="">계약·입주서비스</a>
+                    </li>
+                    <li>
+                        <a href="">고객센터</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="right-button-1">
+                <img src="https://ssw0428.github.io/img1/pf/apt/btn_h_calendar.png" alt="">
+            </div>
+            <div class="right-button-2">
+                <img src="https://ssw0428.github.io/img1/pf/apt/btn_h_mymenu.png" alt="">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
